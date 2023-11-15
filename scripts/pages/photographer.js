86f6f23ab4460ad315469368a2a3d7683cf52c08
gohnzy <gregory.nuzzo@gmail.com>
2023-11-15 01:13:14 +0100

    pc swap

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 //Mettre le code JavaScript lié à la page photographer.html
 
 /* eslint-disable no-undef */
@@ -21,31 +22,35 @@ function displayHeader(photographer) {
     sidePages.appendChild(photographerHeader);
 }
 
+function displayNav() {
+    const insertAfter = document.querySelector(".photographerHeader")
+    const sortMenu = document.createElement("div");
+    sortMenu.classList.add("sortMenu");
+    sortMenu.innerHTML = `<h4>Trier par</h4>
+    <div class="dropdownMenu">
+        <div class="popularDiv"><a id="popular">Popularité<i class="fa-solid fa-chevron-up"></i></a></div>
+        <div class="dateDiv"><a id="date">Date</a></div>
+        <div class="titleDiv"><a id="title">Titre</a></div>
+    </div>`;
+    insertAfter.parentNode.insertBefore(sortMenu, insertAfter.nextElementSibling)
+}
+
 function displayContent(medias, photographer) {
     const {price} = photographer;
     const {likes} = medias;
 
     const sidePages = document.querySelector(".mainSide");
-
-    const sortMenu = document.createElement("div");
-    sortMenu.classList.add("sortMenu");
-    sortMenu.innerHTML = `<h4>Trier par</h4>
-    <div class="dropdownMenu">
-        <a id="popular">Popularité</a><i class="fa-solid fa-chevron-up"></i>
-        <a id="date">Date</a>
-        <a id="title">Titre</a>
-    </div>`;
-    sidePages.appendChild(sortMenu);
     
     const photographerContent = document.createElement("div");
     photographerContent.classList.add("photographerContent");
+    photographerContent.setAttribute("id", "notSorted")
     
     sidePages.appendChild(photographerContent);
 
     let mediaLikesCount = 0;
     let displayLikes = 0
     medias.forEach(media => {
-        
+ 
         const photographPageContent = photographerPageContent(media);
         const photographPageSection = photographPageContent.photographerPageSection();
         
@@ -68,24 +73,31 @@ function displayContent(medias, photographer) {
     toolTip.appendChild(toolTipPrice);
 
     sidePages.appendChild(toolTip); 
-
-    let leek = document.querySelectorAll(".unliked"); 
-        leek.forEach((heart) => heart.addEventListener("click", () => {
-            
-            if(leek.style.color === "#901c1c") {
-                console.log(leek);
-                leek.style.color = "#D3573C";
-
-            }
-            else {
-                leek.style.color = "#901c1c";
-            }
-        }))
-
-            
-       
 }
 
+function displaySortedContent(sortedMedias, photographer) {
+    const {price} = photographer;
+
+    const sidePages = document.querySelector(".mainSide");
+    
+    const photographerContent = document.createElement("div");
+    photographerContent.classList.add("photographerContent");
+    
+    sidePages.appendChild(photographerContent);
+
+    let mediaLikesCount = 0;
+    let displayLikes = 0
+    sortedMedias.forEach(media => {
+ 
+        const photographPageContent = photographerPageContent(media);
+        const photographPageSection = photographPageContent.photographerPageSection();
+        
+        photographerContent.appendChild(photographPageSection);
+        mediaLikesCount += media.likes
+        displayLikes = mediaLikesCount.toLocaleString()
+    });
+
+}
 async function init() {
     const id = new URLSearchParams(window.location.search).get('id');
     const data = await getPhotographersAndMedias();
@@ -101,10 +113,124 @@ async function init() {
     }
 
     const medias = data.media.filter(p => String(p.photographerId) === String(photographer.id)); 
+    
     displayHeader(photographer);
-    displayContent(medias, photographer)
+    displayContent(medias, photographer);
+    displayNav();
     contactForm(photographer);
     submitForm();
+
+    // Sort events
+    const compareContent = document.querySelector(".photographerContent");
+
+    // Sorting by likes (popularity)
+    const popularSort = document.getElementById("popular");
+    popularSort.addEventListener("click", () => {
+        const sortedMedias = Array.from(medias)
+        if (compareContent.id === "notSorted") {
+            sortedMedias.sort(function (a, b) {
+                return b.likes - a.likes;
+            })
+            const toBeDeleted = document.querySelector(".photographerContent");
+            if (toBeDeleted) {
+                toBeDeleted.remove();
+            }
+            displaySortedContent(sortedMedias, photographer);
+            compareContent.setAttribute("id", "popularSorted")
+        }
+
+        else {
+            sortedMedias.sort(function (a, b) {
+                return a.likes - b.likes
+            })
+            const toBeDeleted = document.querySelectorAll(".photographerContent")
+            if (toBeDeleted) {
+                toBeDeleted.forEach(e => {
+                    e.remove()
+                });
+            }
+            displaySortedContent(sortedMedias, photographer);
+            compareContent.setAttribute("id", "notSorted")
+        }
+        
+    })
+
+    // Sorting by date 
+    const dateSort = document.getElementById("date")
+    dateSort.addEventListener("click", () => {
+        const sortedMedias = Array.from(medias)
+        
+        if (compareContent.id === "notSorted") {
+            sortedMedias.sort(function (a, b) {
+                const dateA = new Date(a.date)
+                const dateB = new Date(b.date)
+                return dateA - dateB
+            })
+            const toBeDeleted = document.querySelector(".photographerContent");
+            if (toBeDeleted) {
+                toBeDeleted.remove();
+            }
+            displaySortedContent(sortedMedias, photographer);
+            compareContent.setAttribute("id", "popularSorted")
+        }
+
+        else {
+            sortedMedias.sort(function (a, b) {
+                const dateA = new Date(a.date)
+                const dateB = new Date(b.date)
+                return dateB - dateA
+            })
+            const toBeDeleted = document.querySelectorAll(".photographerContent")
+            if (toBeDeleted) {
+                toBeDeleted.forEach(e => {
+                    e.remove()
+                });
+            }
+            displaySortedContent(sortedMedias, photographer);
+            compareContent.setAttribute("id", "notSorted")
+        }
+
+    })
+
+    // Sorting by title
+    const titleSort = document.getElementById("title");
+    titleSort.addEventListener("click", () => {
+        const sortedMedias = Array.from(medias)
+        if (compareContent.id === "notSorted") {
+            sortedMedias.sort(function (a, b) {
+                if (a.title < b.title) {
+                    return -1
+                }
+
+                return 0
+            })
+            const toBeDeleted = document.querySelector(".photographerContent");
+            if (toBeDeleted) {
+                toBeDeleted.remove();
+            }
+            displaySortedContent(sortedMedias, photographer);
+            compareContent.setAttribute("id", "popularSorted")
+        }
+
+        else {
+            sortedMedias.sort(function (a, b) {
+                if (a.title < b.title) {
+                    return 0
+                }
+
+                return -1
+            })
+            const toBeDeleted = document.querySelectorAll(".photographerContent")
+            if (toBeDeleted) {
+                toBeDeleted.forEach(e => {
+                    e.remove()
+                });
+            }
+            displaySortedContent(sortedMedias, photographer);
+            compareContent.setAttribute("id", "notSorted")
+        }
+        
+    })
 }
 
 init();
@@ -146,7 +272,7 @@ function photographerPageHeader(data) {
 
 function photographerPageContent(data) {
 
-    const {image, video, id, photographerId, title, date, likes} = data;
+    let {image, video, id, photographerId, title, date, likes} = data;
     let file;    
 
     if(image) {
@@ -190,6 +316,19 @@ function photographerPageContent(data) {
         mediaLikes.classList.add("mediaLikes")
         mediaLikes.innerHTML = `${likes} <i class="unliked fa-solid fa-heart"></i>`
         
+        mediaLikes.addEventListener("click", () => {
+            if (mediaLikes.innerHTML === `${likes} <i class="unliked fa-solid fa-heart"></i>`) {
+                likes++
+                mediaLikes.innerHTML = `${likes} <i class="liked fa-solid fa-heart"></i>`;
+
+            }
+            else {
+                likes--
+                mediaLikes.innerHTML = `${likes} <i class="unliked fa-solid fa-heart"></i>`;
+            }
+
+        })
+        
         mediaText.appendChild(mediaLikes);
         
         media.appendChild(mediaText);
@@ -200,4 +339,4 @@ function photographerPageContent(data) {
 
     return {photographerPageSection}
 
-} 
+}   
